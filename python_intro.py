2cda488a8a4ea6301dcd6437209263b5867963f7
marlenap <pozorska.marlena@adres.pl>
2016-10-22 15:42:17 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,22 @@
+# name = 'Natalia'
+# if name =='Ola':
+#     print('Czesc ola')
+# elif name == 'Kasia':
+#     print('Czesc Kasia')
+# else:
+#     print('Hej nieznajoma!')
+
+
+#------------------- Funkcje------------------
+# def hej(imie):
+#     print('Hej ' + imie + '!')
+# hej("Mateusz")
+
+#range-przedział jednstronnnie otwarty, więc range(1,6) - wynik będzie 1 2 3 4 5
+
+def hej(imie):
+    print('Hej ' + imie + '!')
+
+dziewczyny = ['Aleksandra', 'Monica', 'Phoebe', 'Ola', 'Marlena']
+for i in range(1,6):
+    print(i)
